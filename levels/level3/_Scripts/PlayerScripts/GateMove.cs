69d61004cb0e2be4d1f957c929099f39ed8e60dd
LordGibus <springfreddy1@gmail.com>
2026-05-20 02:06:48 -0400

    John 5/20/2026 2 AM
    Somewhat finished "Arena"
    Implemented gate structure to work with a 2D environment
    Added "night sections" for each floor

--- levels/level3/_Scripts/PlayerScripts/GateMove.cs
+++ levels/level3/_Scripts/PlayerScripts/GateMove.cs
@@ -1,4 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GateMove : MonoBehaviour
 {
@@ -13,4 +16,14 @@ public class GateMove : MonoBehaviour
     {
         
     }
+    
+    public string scenename;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+    	if (other.CompareTag("Player"))
+    	{
+        	SceneManager.LoadScene(scenename);
+    	}
+	}
 }
